06c6f683e136e5822fdb7d8bd3aafc6009a230c7
Dmitri Skjorshammer <dmitriskj@gmail.com>
2013-04-27 08:49:34 -0700

    Converted to Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dmitriskj::Application.config.secret_token = 'cc587bc1c789756d9c4eb029a72e2634c53d29274185775f6fc964200312bc582127a9d6f6d5bc7260252ad182bf3e214d4632b0e94601d2539e6b1ad14fdc28'
